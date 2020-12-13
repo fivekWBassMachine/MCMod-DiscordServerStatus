6ae15c69878b6a583bb1681dfb23a5798978ad11
5kWBassMachine <53333237+fivekWBassMachine@users.noreply.github.com>
2020-12-13 23:18:26 +0100

    Optimized imports

--- src/main/java/com/github/fivekwbassmachine/minecraft/discordserverstatus/DiscordServerStatus.java
+++ src/main/java/com/github/fivekwbassmachine/minecraft/discordserverstatus/DiscordServerStatus.java
@@ -2,13 +2,12 @@ package com.github.fivekwbassmachine.minecraft.discordserverstatus;
 
 import com.github.fivekwbassmachine.minecraft.discordserverstatus.minecraft.commands.DiscordStatus;
 import cpw.mods.fml.common.FMLCommonHandler;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.event.FMLServerStartedEvent;
 import cpw.mods.fml.common.event.FMLServerStartingEvent;
-import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.Mod.EventHandler;
-import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
